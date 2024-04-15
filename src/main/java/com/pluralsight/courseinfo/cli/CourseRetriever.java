15bad5fa5642c975e735d7cfcd9df6ba2f19c24c
Igor-GF <igfigueiredo@outlook.com>
2024-04-15 16:41:24 +0200

    Courses fetched as Records by using Jackson dependency

--- src/main/java/com/pluralsight/courseinfo/cli/CourseRetriever.java
+++ src/main/java/com/pluralsight/courseinfo/cli/CourseRetriever.java
@@ -1,8 +1,11 @@
 package com.pluralsight.courseinfo.cli;
 
 import com.pluralsight.courseinfo.cli.service.CourseRetrievalService;
+import com.pluralsight.courseinfo.cli.service.PluralsightCourse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.List;
 
 public class CourseRetriever {
     private static final Logger LOG = LoggerFactory.getLogger(CourseRetriever.class);
@@ -25,7 +28,10 @@ public class CourseRetriever {
         LOG.info("Retrieving courses for author '{}'...", authorId);
         CourseRetrievalService courseRetrievalService = new CourseRetrievalService();
 
-        String coursesToStore = courseRetrievalService.getCoursesFor(authorId);
-        LOG.info("Retrieved courses {}", coursesToStore);
+        List<PluralsightCourse> coursesToStore = courseRetrievalService.getCoursesFor(authorId)
+                .stream()
+                .filter(c -> !c.isRetired())
+                .toList();
+        LOG.info("Retrieved {} courses {}", coursesToStore.size(), coursesToStore);
     }
 }
